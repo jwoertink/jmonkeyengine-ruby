b596a286e5c2da2bc6cc3e5c798abedb4e9139cc
Jeremy Woertink <jeremywoertink@gmail.com>
2015-01-30 08:50:56 -0800

    bump patch for making the gem smaller

--- lib/jmonkeyengine/version.rb
+++ lib/jmonkeyengine/version.rb
@@ -1,5 +1,5 @@
 module JMonkeyEngine
-  VERSION = "0.1.0" # Version of the Gem
+  VERSION = "0.1.1" # Version of the Gem
   JME_VERSION = "3.0" # This is the stable framework version
   JAR_VERSION = "2014-06-12" # Version of the actual .jar file
 end
